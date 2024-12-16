2efe6928e8c41c60ca395656cea40ccfd14baa0d
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-12-16 14:50:19 -0700

    Minor tweaks to the copy written in the Home component.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,7 @@ function Home() {
                 <div id="portfolio-pitch">
                     <h1>Eric Hess</h1>
                     <h2>Astrophysicist | Full-Stack Engineer</h2>
-                    <p>Bridging astrophysics and the magic of full-stack developement. My work has sharpened my ability to handle large datasets, write efficient algorithms, and uncover insights in dynamic, data-rich environments.</p>
+                    <p>I'm passionate about building robust projects like a full-stack developer, motivated by research goals in physics and astrophysics. My work has enabled telescope operators (like me!) and researchers to handle large datasets, write efficient algorithms, and uncover insights in dynamic, data-rich environments.</p>
                     <p>Check out some of my most recent projects below!</p>
                 </div>
             </section>
