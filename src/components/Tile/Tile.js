4a4b53ade836c33f46c30227271959d63b7fe528
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-12-17 13:40:32 -0700

    Added more publications, changed year and month format to str

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -5,6 +5,7 @@ function Tile({data}) {
     return (
         <div className={styles.tile}>
             <h1 className={styles.tileHeader}>{data.title}</h1>
+            <p>Date: {data.year}-{data.month}</p>
             <p>{data.description}</p>
             <a href={data.url} target="_blank" rel="noreferrer">View on {data.hostedBy}</a>
         </div>
