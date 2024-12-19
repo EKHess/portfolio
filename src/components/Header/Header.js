4229e18596e75f4f4082e065b60b35a5c27b3a8a
Eric Hess <110352704+EKHess@users.noreply.github.com>
2024-12-19 09:58:33 -0700

    Created separate navigation bars for mobile and desktop users.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 
 function Header() {
     return (
         <>
-            <div className="nav-container">
+            <div className={styles.navContainerDesktop}>
                 <NavLink to="/home" className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink>
                 <NavLink to="/portfolio">Portfolio</NavLink>
                 <NavLink to="/publications">Publications</NavLink>
+            </div>
+            <div className={styles.navContainerMobile}>
+                <NavLink><ion-icon name="chevron-back-outline"></ion-icon></NavLink>
+                <NavLink><ion-icon name="menu-outline"></ion-icon></NavLink>
             </div>
         </>
     )
