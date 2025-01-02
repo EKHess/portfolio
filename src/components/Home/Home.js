1255ca4d1ea4159dfe41f21cdf3c104278c81749
Eric Hess <110352704+EKHess@users.noreply.github.com>
2025-01-01 18:26:03 -0700

    Styled Home component and main content of the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -11,11 +11,14 @@ function Home() {
                 <div className={styles.bannerImageContainer}>
                     <img className={styles.img} src={image} alt="Selfie of Eric with the Baker-Nunn Telescope"/>
                 </div>
-                <div id="portfolio-pitch">
-                    <h1>Eric Hess</h1>
-                    <h2>Astrophysicist | Full-Stack Engineer</h2>
-                    <p>I'm passionate about building robust projects like a full-stack developer, motivated by research goals in physics and astrophysics. My work has enabled telescope operators (like me!) and researchers to handle large datasets, write efficient algorithms, and uncover insights in dynamic, data-rich environments.</p>
-                    <p>Check out some of my most recent projects below!</p>
+                <div className={styles.bioSection}>
+                    <h1 className={styles.name}>Eric Hess</h1>
+                    <h2 className={styles.skills}>Astrophysicist & Full-Stack Engineer</h2>
+                    <div className={styles.bioText}>
+                        <p>I'm passionate about building robust projects like a full-stack developer, motivated by research goals in physics and astrophysics. My work has enabled telescope operators and researchers (like me!) to handle large datasets, write efficient algorithms, and uncover insights in dynamic, data-rich environments.</p>
+                        <p>Check out some of my most recent projects below!</p>
+                    </div>
+
                 </div>
             </section>
 
