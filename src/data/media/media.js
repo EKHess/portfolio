14d2b02228d5d921c73350b3ad6633dc373d160e
Eric Hess <110352704+EKHess@users.noreply.github.com>
2026-06-08 12:15:54 -0600

    Created new media.js data file.

---
+++ src/data/media/media.js
@@ -0,0 +1,31 @@
+export const portfolioLabel = "Eric's Media Appearances";
+
+export const projects = [
+    {
+        id: 0,
+        title: "Meet Eric Hess - UCalgary Science Careers",
+        description: "Meet Eric Hess! An astrophysics major who did his co-op at the Rothney Astrophysical Observatory. Hear what he has to say about his experience in the program and in his co-op. ☄️✨ \nThe possibilities with the Science Co-op Program are endless! 💫🌠 \nAre you interested in the program, but missed the deadline? Check out our upcoming workshops on Elevate to prepare for the next admission cycle in the Fall! 🍂🥼\nIf you have any questions or a specific circumstance, be sure to email us at science.careers@ucalgary.ca.",
+        year: "2025",
+        month: "03",
+        url: "https://www.instagram.com/reels/DHbh2c9PgXI/",
+        hostedBy: "Instagram",
+    },
+    {
+        id: 1,
+        title: "'Amazing experience': Calgarians bask in partial solar eclipse at viewings across the city",
+        description: "Eric Hess, an astrophysics intern at Rothney Astrophysical Observatory, said this was the biggest event he’s been a part of.\nHess believes eclipses bring people out of their day-to-day lives and get them to look up at the sky.\n\"It connects them with nature outside of just our planet,” he said. “If we could just get people interested in the sky today, that would be awesome.\"\nHe said total solar eclipses are helpful when studying the composition of the sun.\n\"During a solar eclipse, if you’re under the path of totality you can take a spectrum of the sun and you’ll learn what the atmosphere of the sun’s made of,” he said.\"\n\"Every single solar eclipse is an opportunity to learn more.\"",
+        year: "2024",
+        month: "04",
+        url: "https://calgaryherald.com/news/calgarians-prepared-for-solar-eclipse-viewings-across-the-city",
+        hostedBy: "Calgary Herald",
+    },
+    {
+        id: 2,
+        title: "'It does kind of put you in your place': Calgary comes out to catch glimpse of solar eclipse",
+        description: "A rare solar eclipse was visible here in Calgary, albeit only partly. But that didn't stop many from seeking out a better look.",
+        year: "2024",
+        month: "04",
+        url: "https://www.ctvnews.ca/calgary/article/it-does-kind-of-put-you-in-your-place-calgary-comes-out-to-catch-glimpse-of-solar-eclipse/",
+        hostedBy: "Global News",
+    },
+]
